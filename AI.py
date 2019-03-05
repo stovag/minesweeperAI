714aba8b2a03edd486c4dab461d5ce611f61a8f3
stovag <44883047+stovag@users.noreply.github.com>
2019-03-05 10:29:55 +0200

    AI now works sometimes, can't handle randomness
    Can't open tiles if there is a chance that they're bombs (goes into an infinite loop).

--- AI.py
+++ AI.py
@@ -1,76 +1,103 @@
-from string import ascii_lowercase
 import random
+from string import ascii_lowercase
 
 
-def mineAI(board, size):
+def mineAI(currgrid, size):
+
+    input()
+
+    #Create an empty Board
+    board = []
+    for i in range(size):
+        board.append([])
+        for j in range(size):
+            board[i].append([])
+
+    #Fill the board with the numbers from the currgrid or with " "
+    for i in range(size):
+        for j in range(size):
+            board[i][j] = str(currgrid[i][j])
+
     #Check if there are open blocks
-    S_open = 0;
+    S_open = 0
     for i in range(size):
         for j in range(size):
             if(board[i][j] != ' '):
-                S_open += 1;
+                S_open += 1
 
     if(S_open == 0): #If all blocks are closed open the first one
         return 'a1'
     else:
-        #For every block try to solve it
-        for i in range(size):
-            for j in range(size):
-                if(board[i][j] != ' ') and (board[i][j] != 'F') and (board[i][j] != '0'):
-                    #Check all neighbors
-                    hidden_neighbors = check_neighbors(board,i,j,size);
-                    flagged_neighbors = check_flags(board, i, j, size);
+        while True:
+            #For every block try to solve it
+            for i in range(size):
+                for j in range(size):
+                    if(board[i][j] != ' ') and (board[i][j] != 'F') and (board[i][j] != '0'):
+                        #Check all neighborsi
+                        hidden_neighbors = check_neighbors(board, i, j, size)
+                        flagged_neighbors = check_flags(board, i, j, size)
 
-                    #Flagging
-                    if(hidden_neighbors == int(board[i][j])):
-                        print(flag_neighbors(board, i, j, size))
-                        return flag_neighbors(board,i,j,size);
-                    #Open safe closed
-                    elif(flagged_neighbors == int(board[i][j])) and (hidden_neighbors != 0):
-                        print(open_neighbors(board, i, j, size));
-                        return open_neighbors(board, i, j, size);
-                    else:
-                        while(True):
-                            y = random.randrange(j-1, j+1)
-                            x = random.randrange(i-1, i+1)
-                            if((x > 0) and (x < size) and (y > 0) and (y < size)):
-                                return '{}{}'.format(ascii_lowercase[y], x)
-                                break;
+                        if hidden_neighbors > 0:
+                            #Open safe closed
+                            if(flagged_neighbors == int(board[i][j])):
+                                return open_neighbors(board, i, j, size)
+                            #Flagging
+                            elif hidden_neighbors == int(board[i][j]) or hidden_neighbors+flagged_neighbors == int(board[i][j]):
+                                return flag_neighbors(board, i, j, size)
+                            else:
+                                break
+        return "You suck at coding 1"
+
 
 #Check for every neighbor if they're blank
 def check_neighbors(board, i, j, size):
-    s_hidden = 0;
-    for x in range(i-1,i+1):
-        for y in range(j-1,j+1):
-            if((x >= 0) and (x <= size) and (y >= 0) and (y <= size)):
-                if(board[x][y] == ' '):
-                    s_hidden += 1;
-    return s_hidden;
+    s_hidden = 0
+    for x in range(i-1,i+2):
+        for y in range(j-1,j+2):
+            try:
+                if((x >= 0) and (x <= size) and (y >= 0) and (y <= size)):
+                    if board[x][y] == " ":
+                        s_hidden += 1
+            except:
+                break
+    return s_hidden
+
 
 #Check for every neighborif they're flagged
 def check_flags(board, i, j, size):
-    s_flagged = 0;
-    for x in range(i-1,i+1):
-        for y in range(j-1,j+1):
-            if((x >= 0) and (x <= size) and (y >= 0) and (y <= size)):
-                if(board[x][y] == 'F'):
-                    s_flagged += 1;
-    return s_flagged;
+    s_flagged = 0
+    for x in range(i-1,i+2):
+        for y in range(j-1,j+2):
+            try :
+                if((x >= 0) and (x <= size-1) and (y >= 0) and (y <= size-1)):
+                    if board[x][y] == "F":
+                        s_flagged += 1
+            except:
+                break
+    return s_flagged
+
 
 #Flag near blank neighbors
 def flag_neighbors(board,i,j,size):
-    for x in range(i-1,i+1):
-        for y in range(j-1, j+1):
-            if((x >= 0) and (x <= size) and (y >= 0) and (y <= size)):
-                if(board[x][y] == ' '):
-                    print('{}{}f'.format(ascii_lowercase[y],x));
-                    return '{}{}f'.format(ascii_lowercase[y],x);
+    for x in range(i-1, i+2):
+        for y in range(j-1, j+2):
+            try:
+                if((x >= 0) and (x <= size) and (y >= 0) and (y <= size)):
+                    if board[x][y] == " ":
+                        return ('{}{}f'.format(ascii_lowercase[y],x+1))
+            except:
+                break
+    return "You suck at coding 2"
+
 
 #Open safe closed
-def open_neighbors(board,i,j,size):
-    for x in range(i-1,i+1):
-        for y in range(j-1, j+1):
-            if((x >= 0) and (x <= size) and (y >= 0) and (y <= size)):
-                if(board[x][y] == ' '):
-                    print('{}{}'.format(ascii_lowercase[y],x));
-                    return '{}{}'.format(ascii_lowercase[y],x);
+def open_neighbors(board, i, j, size):
+    for x in range(i-1,i+2):
+        for y in range(j-1, j+2):
+            try:
+                if((x >= 0) and (x <= size) and (y >= 0) and (y <= size)):
+                    if board[x][y] == " ":
+                        return ('{}{}'.format(ascii_lowercase[y],x+1))
+            except:
+                break
+    return "You suck at coding 3"
